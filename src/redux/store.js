92404b11aa133817b32e6fc312268f712dbf4302
Tetita <89341644+Tetita@users.noreply.github.com>
2022-12-12 05:50:45 -0600

    Initialize change-userShouldBeSearched-variable

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import adminReducer from './slices/adminSlice';
+import channelCreationReducer from './slices/channelCreationSlice';
+import channelReducer from './slices/channelSlice';
+import chatReducer from './slices/chatSlice';
+import contractReducer from './slices/contractSlice';
+import chatReducer from "./slices/chatSlice";
+import notificationReducer from './slices/notificationSlice';
+import canSendNotification from "./slices/sendNotificationSlice";
+import spamReducer from './slices/spamSlice';
+import userJourneyReducer from './slices/userJourneySlice';
+
+const rootReducer = combineReducers({
+	contracts: contractReducer,
+	channels: channelReducer,
+	chat: chatReducer,
+	channelCreation: channelCreationReducer,
+	admin: adminReducer,
+	notifications: notificationReducer,
+	spam: spamReducer,
+	userJourney: userJourneyReducer,
+	canSend:canSendNotification,
+});
+
+const store = configureStore({
+	reducer: rootReducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: false,
+		immutableCheck: false
+	})
+});
+
+export default store;
