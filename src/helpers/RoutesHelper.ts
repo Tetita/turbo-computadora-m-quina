86d8ba5cbdcba9d58416099ed112d336b82c4ecd
Tetita <89341644+Tetita@users.noreply.github.com>
2022-12-12 05:50:00 -0600

    Initialize fix-ens-sidebar

---
+++ src/helpers/RoutesHelper.ts
@@ -0,0 +1,16 @@
+import { appConfig } from "config";
+
+const apiVersion: number = appConfig.pushNodeApiVersion;
+const channelsRoute: string = `/v${apiVersion}/channels`;
+const usersRoute: string = `/v${apiVersion}/users`;
+
+export const usersServiceEndpoints = {
+    userSubscriptions: (userAddressInCAIP: string) => `${usersRoute}/${userAddressInCAIP}/subscriptions`,
+    userDelegations: (userAddressInCAIP: string) => `${usersRoute}/${userAddressInCAIP}/delegations`
+}
+
+export const channelsServiceEndpoints = {
+    channelsInfo: (queryParams?: string) => `${channelsRoute}?${queryParams}`,
+    channelsSearch: (queryParams: string) => `${channelsRoute}/search?${queryParams}`,
+    channelDelegates: (channelAddressInCAIP: string) => `${channelsRoute}/${channelAddressInCAIP}/delegates`
+}
